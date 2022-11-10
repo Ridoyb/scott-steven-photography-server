75723bcb4b967ab9bfda918a5b56ec6bf8d1d5e6
Ridoyb <ridoyb8730@gmail.com>
2022-11-10 22:53:00 +0600

    data added to mongodb

--- index.js
+++ index.js
@@ -13,13 +13,86 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+// const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.twtll.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.17mhv8n.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('scottPgotography').collection('services');
+        const reviewsCollection = client.db('scottPgotography').collection('reviews');
+
+        app.get('/services-home', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query).sort({"_id":-01});
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+
+       
+
+
+        // reviews
+    app.get("/reviews", async (req, res) => {
+        let query = {};
+        if (req.query.email) {
+          query = {
+            email: req.query.email,
+          };
+        }
+        const cursor = reviewsCollection.find(query).sort({"_id":-01});
+        const reviews = await cursor.toArray();
+        res.send(reviews);
+      });
+
+
+    app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await reviewsCollection.findOne(query);
+            res.send(service);
+        });
+  
+      // add new review
+      app.post("/add-review", async (req, res) => {
+        const review = req.body;
+        const result = await reviewsCollection.insertOne(review);
+        res.send(result);
+      });
+
+      //Edit review
+      app.put('/reviews/:id', async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: ObjectId(id) };
+        const status = req.body;
+        const option = {upsert: true};
+        const updatedReview = {
+            $set: {
+                review: status.review
+            }
+        }
+        const result = await reviewsCollection.updateOne(filter, updatedReview, option);
+        res.send(result);
+    })
+
+
+    //Add Service
+    app.post("/services", async (req, res) => {
+        const review = req.body;
+        const result = await serviceCollection.insertOne(review);
+        res.send(result);
+      });
+    } 
+    finally {
+
+    }
+
+}
+
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
